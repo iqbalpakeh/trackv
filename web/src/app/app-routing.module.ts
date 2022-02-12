89773fda4b8dccd8a157bb37d9ddc575c916522e
Progrema Studio <iqbalpakeh@gmail.com>
2022-02-12 20:18:54 +0800

    Add router (not working)

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IssueDetailsComponent } from './reusables/issue-details/issue-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'issues/:issueId', component: IssueDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- web/src/app/reusables/issue-details/issue-details.component.ts
+++ web/src/app/reusables/issue-details/issue-details.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Issue, issues } from '../../data/issue-samples';
 
 @Component({
   selector: 'app-issue-details',
   templateUrl: './issue-details.component.html',
-  styleUrls: ['./issue-details.component.css']
+  styleUrls: ['./issue-details.component.css'],
 })
 export class IssueDetailsComponent implements OnInit {
+  issue: Issue | undefined;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-  }
+    const routeParams = this.route.snapshot.paramMap;
+    const issueIdFromRoute = routeParams.get('issueId');
 
+    console.log(routeParams);
+    console.log(`id: ${issueIdFromRoute}`);
+
+    this.issue = issues.find((issue) => issue.id === issueIdFromRoute);
+
+    // this.issue = {
+    //   id: 'ID0002',
+    //   short: 'HELLO',
+    // };
+  }
 }
