490d275725441625c3527600c1f39d82157a4c01
Progrema Studio <iqbalpakeh@gmail.com>
2022-02-12 11:35:12 +0800

    move value out

--- web/src/app/reusables/issue-short/issue-short.component.ts
+++ web/src/app/reusables/issue-short/issue-short.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-issue-short',
   templateUrl: './issue-short.component.html',
-  styleUrls: ['./issue-short.component.css']
+  styleUrls: ['./issue-short.component.css'],
 })
 export class IssueShortComponent implements OnInit {
+  id = 'ID0001X';
 
-  constructor() { }
+  describtion =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quidem asperiores nihil nisi optio mollitia? Nulla, consectetur quam provident odio nobis vel exercitationem rem aperiam molestias minima natus qui perferendis.';
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
