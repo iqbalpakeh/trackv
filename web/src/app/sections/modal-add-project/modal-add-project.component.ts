d302445067d369f7fd5eefbff0f1c4ee5619deb9
Progrema Studio <iqbalpakeh@gmail.com>
2022-02-13 17:27:28 +0800

    Closed the modal

--- web/src/app/sections/modal-add-project/modal-add-project.component.ts
+++ web/src/app/sections/modal-add-project/modal-add-project.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-modal-add-project',
   templateUrl: './modal-add-project.component.html',
-  styleUrls: ['./modal-add-project.component.css']
+  styleUrls: ['./modal-add-project.component.css'],
 })
 export class ModalAddProjectComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  cancel() {
+    this.closePopup();
   }
 
+  closePopup() {
+    // Providing a `null` value to the named outlet
+    // clears the contents of the named outlet
+    this.router.navigate([{ outlets: { addProject: null } }]);
+  }
 }
