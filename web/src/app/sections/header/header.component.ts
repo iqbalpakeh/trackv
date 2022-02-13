eeae273a8ecce9871435573bd723ad6942bb99f6
Progrema Studio <iqbalpakeh@gmail.com>
2022-02-13 09:55:09 +0800

    control modals

--- web/src/app/sections/header/header.component.ts
+++ web/src/app/sections/header/header.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  showAddProjectModal = false;
+  showAddIssueModal = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  addProject() {
+    this.showAddProjectModal = true;
   }
 
+  addIssue() {
+    this.showAddIssueModal = true;
+  }
 }
