1cf4f0b0b5628ef8eaa41417b00836666abf5c7f
Progrema Studio <iqbalpakeh@gmail.com>
2022-02-13 16:45:19 +0800

    Manage to call add project

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { IssueDetailsComponent } from './reusables/issue-details/issue-details.component';
+import { ModalAddProjectComponent } from './sections/modal-add-project/modal-add-project.component';
 
 const routes: Routes = [
   { path: 'issues/:issueId', component: IssueDetailsComponent },
+  {
+    path: 'projects',
+    component: ModalAddProjectComponent,
+    outlet: 'addProject',
+  },
 ];
 
 @NgModule({
